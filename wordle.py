fa6067ba95033b2f4e72d187fe3918fbd8122ce8
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-06-14 21:32:08 +0100

    wrapped vocab and hist in functions

--- wordle.py
+++ wordle.py
@@ -1,20 +1,22 @@
 import pandas as pd 
 # import nltk 
 # nltk.download('brown')
-# from nltk.corpus import brown
+from nltk.corpus import brown
 from random import sample 
 
-# vocab = sorted(list(set([w.upper() for w in brown.words() if len(w)==5 and w[0].islower() and w.isalpha()])))
-vocab = pd.read_csv('english-words.txt')
-vocab = vocab['a'].tolist()
-vocab = [w.upper() for w in vocab if len(str(w))==5]
 
-# print(vocab[:10])
+def historical_answers():
+    f = pd.read_csv('prev-answers.txt') 
+    history = f.columns.tolist()
+    history = history[0].split(sep=" ")
+    return history
 
-
-f = pd.read_csv('prev-answers.txt') 
-history = f.columns.tolist()
-history = history[0].split(sep=" ")
+def get_vocab(): 
+    # vocab = sorted(list(set([w.upper() for w in brown.words() if len(w)==5 and w[0].islower() and w.isalpha()])))
+    vocab = pd.read_csv('english-words.txt')
+    vocab = vocab['a'].tolist()
+    vocab = [w.upper() for w in vocab if len(str(w))==5]
+    return vocab
 
 def check_word(word, history): 
     return 'new word' if word not in history else 'already used'
@@ -37,20 +39,25 @@ def word_without_char(vocab, history, char):
     return subset
 
 
+vocab = get_vocab() 
+history = historical_answers()
 # print(generate_word(vocab, history))
 
-results = word_with_char(vocab=vocab, history=history, char='s', char_idx=3)
-results = word_with_char(vocab=results, history=history, char='t', char_idx=4)
-results = word_with_char(vocab=results, history=history, char='a', not_char_idx=2)
-results = word_with_char(vocab=results, history=history, char='a', not_char_idx=1)
+results = word_with_char(vocab=vocab, history=history, char='l', not_char_idx=1)
+# results = word_with_char(vocab=results, history=history, char='t', char_idx=4)
+# results = word_with_char(vocab=results, history=history, char='a', not_char_idx=2)
+# results = word_with_char(vocab=results, history=history, char='a', not_char_idx=1)
 # results = word_with_char(vocab=results, history=history, char='e', not_char_idx=4)
 
-results = word_without_char(results, history, 'l')
+results = word_without_char(results, history, 's')
+results = word_without_char(results, history, 'c')
+results = word_without_char(results, history, 'o')
+results = word_without_char(results, history, 'r')
 results = word_without_char(results, history, 'e')
-results = word_without_char(results, history, 'w')
+results = word_without_char(results, history, 'b')
 results = word_without_char(results, history, 'i')
-# results = word_without_char(results, history, 'c')
-# results = word_without_char(results, history, 'h')
-# results = word_without_char(results, history, 'y')
-# results = word_without_char(results, history, 'r')
+results = word_without_char(results, history, 'n')
+results = word_without_char(results, history, 'd')
 print(results)
+
+# print(check_word('score', history))
